3f705d5d821f03f5209fb2c8432eb1c97a436a49
NG <guraband@naver.com>
2023-10-24 21:26:31 +0900

    프로젝트 세팅 & Flux, Mono 기초 예제 추가

---
+++ src/main/java/fastcampus/backendsignature/webflux/Publisher.java
@@ -0,0 +1,16 @@
+package fastcampus.backendsignature.webflux;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Publisher {
+    public Flux<Integer> startFlux() {
+        return Flux.range(1, 10)
+                .log();
+    }
+
+    public Mono<Integer> startMono() {
+        return Mono.just(1)
+                .log();
+    }
+}

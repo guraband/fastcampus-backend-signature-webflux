49f79d21fc68c6e0d383b38c64809100909f58d0
NG <guraband@naver.com>
2023-10-29 00:05:33 +0900

    Operator 예제 따라하기

---
+++ src/test/java/fastcampus/backendsignature/webflux/Operator1Test.java
@@ -0,0 +1,44 @@
+package fastcampus.backendsignature.webflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+class Operator1Test {
+
+    private Operator1 operator = new Operator1();
+
+    @Test
+    void fluxMap() {
+        StepVerifier.create(operator.fluxMap())
+                .expectNext(2, 4, 6, 8, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFilter() {
+        StepVerifier.create(operator.fluxFilter())
+                .expectNext(6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxTake() {
+        StepVerifier.create(operator.fluxTake())
+                .expectNext(6, 7, 8)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFlatMap() {
+        StepVerifier.create(operator.fluxFlatMap())
+                .expectNextCount(100)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFlatMap2() {
+        StepVerifier.create(operator.fluxFlatMap2())
+                .expectNextCount(81)
+                .verifyComplete();
+    }
+}

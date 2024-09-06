0c868f13683f3e44a60b3fd81f0cd5982ac8e38c
NG <guraband@naver.com>
2024-09-06 23:40:05 +0900

    진입 페이지 연동

--- queue-web/src/main/java/fastcampus/backendsignature/webflux/queueweb/contoller/WebController.java
+++ queue-web/src/main/java/fastcampus/backendsignature/webflux/queueweb/contoller/WebController.java
@@ -1,14 +1,59 @@
 package fastcampus.backendsignature.webflux.queueweb.contoller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("")
 public class WebController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping({"", "/"})
-    public String index() {
+    public String index(
+            @RequestParam(name = "queue", defaultValue = "default") String queue,
+            @RequestParam(name = "user_id") Long userId,
+            HttpServletRequest request
+    ) {
+        var cookies = request.getCookies();
+        var cookieName = "user-queue-%s-token".formatted(queue);
+
+        var token = "";
+        if (cookies != null) {
+            token = Arrays.stream(cookies)
+                    .filter(i -> i.getName().equals(cookieName))
+                    .findFirst()
+                    .orElse(new Cookie(cookieName, ""))
+                    .getValue();
+        }
+        var uri =
+                UriComponentsBuilder
+                        .fromHttpUrl("http://127.0.0.1:9010")
+                        .path("/api/v1/queue/allowed")
+                        .queryParam("queue", queue)
+                        .queryParam("user_id", userId)
+                        .queryParam("token", token)
+                        .encode()
+                        .build()
+                        .toUri();
+
+        ResponseEntity<AllowedUserResponse> response = restTemplate.getForEntity(uri, AllowedUserResponse.class);
+        if (response.getBody() == null || !response.getBody().isAllowed()) {
+            return "redirect:http://127.0.0.1:9010/waiting-room?queue=%s&user_id=%d&redirect_url=%s"
+                    .formatted(queue, userId, "http://127.0.0.1:9000?user_id=%d".formatted(userId));
+        }
         return "index";
+    }
+
+    public record AllowedUserResponse(Boolean isAllowed) {
     }
 }
